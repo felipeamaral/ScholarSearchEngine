e5d3ac12a4264596d8647f7a77700952c09bcfe5
vanessagomes <vane.gomesl@gmail.com>
2014-03-10 18:05:32 +0000

    Changing print to "agora vai".

--- SearchEngine/src/Main.java
+++ SearchEngine/src/Main.java
@@ -3,8 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Vanessa!!");
-		System.out.println("hahahaha");
+		System.out.println("Agora vai!");
 	}
 
 }
