9b91d9ed2a1c13e809f9854e1ef5416418b3d8a8
felipeaamaral <amaral.felipeaugusto@gmail.com>
2014-03-21 17:25:53 +0000

    - PDFParser - Metadata dividido

--- SearchEngine/src/qmul/test/tika/sample/ParseIndex.java
+++ SearchEngine/src/qmul/test/tika/sample/ParseIndex.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -17,82 +18,88 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.pdf.PDFParser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
- 
+
 public class ParseIndex {
- 
+
 	public static final String INDEX_DIRECTORY = "index/ramdisk/";
 	public static int writeLimit = -1;
-	
-    public static void main(String[] args) throws IOException{
-    	
-    	File docs = new File("documents");
-    	File indexDir = new File(INDEX_DIRECTORY);
-    	
-    	Directory dir = FSDirectory.open(indexDir);
-    	
 
-    	Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
-    	IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
-    	IndexWriter writer  = new IndexWriter(dir, config);
-    	writer.deleteAll();
-    	
-    	for(File file : docs.listFiles()){
-    		Metadata metadata = new Metadata();
-    		ContentHandler handler = new BodyContentHandler(writeLimit);
-    		ParseContext context = new ParseContext();
-    		Parser parser = new AutoDetectParser();
-    		
-    		InputStream inputStream = new FileInputStream(file);
-    		
-    		try{
-    			parser.parse(inputStream, handler, metadata, context);
-    		} catch(TikaException te){
-    			te.printStackTrace();
-    		} catch (SAXException se) {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws IOException {
+		long t1 = System.currentTimeMillis();
+		File docs = new File("documents");
+		File indexDir = new File(INDEX_DIRECTORY);
+
+		Directory dir = FSDirectory.open(indexDir);
+
+		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40,
+				analyzer);
+		IndexWriter writer = new IndexWriter(dir, config);
+		writer.deleteAll();
+
+		for (File file : docs.listFiles()) {
+			Metadata metadata = new Metadata();
+			ContentHandler handler = new BodyContentHandler(writeLimit);
+			ParseContext context = new ParseContext();
+			Parser parser = new PDFParser();
+
+			InputStream inputStream = new FileInputStream(file);
+
+			try {
+				parser.parse(inputStream, handler, metadata, context);
+			} catch (TikaException te) {
+				te.printStackTrace();
+			} catch (SAXException se) {
 				se.printStackTrace();
 			} finally {
 				inputStream.close();
 			}
-    		
-    		String text = handler.toString();
-    		String fileName = file.getName();
-    		
-    		Document doc = new Document();
-    		doc.add(new StringField("file", fileName, Store.YES));
-    		
-    		for(String key : metadata.names()){
-    			String name = key.toLowerCase();
-    			String values = metadata.get(key);
-    			
-    			System.out.println("name: " + name);
-    			System.out.println("key       " + key);
-    			System.out.println("values    " + values);
-    			
-    			if("keywords".equalsIgnoreCase(key)){
-    				for(String keyword : values.split(",?(\\s+)")){
-    					doc.add(new TextField(name, keyword, Store.YES));
-    				}
-    			} else if("title".equalsIgnoreCase(key)){
-    				doc.add(new TextField(name, values, Store.YES));
-    			} else if("author".equalsIgnoreCase(key)){
-    				doc.add(new StringField(name, values, Store.YES));
-    			}
-    		}
-    		
-    		doc.add(new TextField("text", text, Store.NO));
-    		writer.addDocument(doc);
-    		
-    		
-    	}
-    	
-    	writer.commit();
-    	writer.deleteUnusedFiles();
-    	System.out.println(writer.maxDoc() + " documents written");
-    }
+
+			String text = handler.toString();
+			String fileName = file.getName();
+
+			Document doc = new Document();
+			doc.add(new StringField("file", fileName, Store.YES));
+
+			System.out.println("Filename:    " + fileName);
+			String values = metadata.get("Keywords");
+			if (values != null) {
+				System.out.println("Keywords values:    " + values);
+				for (String keyword : values.split(",?(\\s+)")) {
+					doc.add(new TextField("keywords", keyword, Store.YES));
+				}
+			}
+
+			values = metadata.get("title");
+			if (values != null) {
+				System.out.println(" Title value:    " + values);
+				doc.add(new TextField("title", values, Store.YES));
+			}
+
+			values = metadata.get("Author");
+			if (values != null) {
+				System.out.println("Author value:    " + values);
+				doc.add(new StringField("author", values, Store.YES));
+			}
+
+			doc.add(new TextField("text", text, Store.NO));
+			writer.addDocument(doc);
+			System.out.println();
+
+		}
+
+		writer.commit();
+		writer.deleteUnusedFiles();
+		long t2 = System.currentTimeMillis();
+
+		System.out.println(writer.maxDoc() + " documents written");
+		System.out.println("Time consumed(ms): " + (t2 - t1));
+	}
 }
