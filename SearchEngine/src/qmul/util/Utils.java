6f784ef84d3c7f3d8975aaee1fc85abdb78b40a4
felipeaamaral <amaral.felipeaugusto@gmail.com>
2014-03-22 18:28:36 +0000

    Package Utils - Constants.

---
+++ SearchEngine/src/qmul/util/Utils.java
@@ -0,0 +1,15 @@
+package qmul.util;
+
+import java.util.Set;
+
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.util.Version;
+
+public class Utils {
+	
+	public static final String INDEX_DIRECTORY = "index/ramdisk/";
+	public static int writeLimit = -1;
+	public final static Version LUCENE_VERSION = Version.LUCENE_40;
+	
+	
+}
