88fcb8f8ae53d43e273365279faad04a47e4d242
vanessagomes <vane.gomesl@gmail.com>
2014-03-24 17:23:54 +0000

    Simple class that will represent the ranking of the search.

---
+++ SearchEngine/src/qmul/lucene/search/Ranking.java
@@ -0,0 +1,44 @@
+package qmul.lucene.search;
+
+import java.util.ArrayList;
+
+import org.apache.lucene.document.Document;
+
+public class Ranking {
+	private ArrayList<Document> ranking;
+	
+	public Ranking() {
+		ranking = new ArrayList<Document>();
+	}
+	
+	/**
+	 * Add a document to the end of the list.
+	 * @param doc
+	 */
+	public void addDocument(Document doc){
+		ranking.add(doc);
+	}
+	
+	/**
+	 * Gets the ranking.
+	 * @return the ranking
+	 */
+	public ArrayList<Document> getRanking() {
+		return ranking;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		String rankingText = "";
+		
+		for (Document doc : ranking) {
+			rankingText = rankingText + doc.get("file");
+			rankingText = rankingText + "\nTitle: " + doc.get("title");
+			rankingText = rankingText + "\n\n";
+		}
+		return rankingText;
+	}
+}
